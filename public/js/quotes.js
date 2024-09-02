b1967847e4e7b1c6a162c5bde09866f3c51704f8
Nathan <NathanGoldbaum@ctulocal1.org>
2024-09-02 05:12:45 -0500

    Got animations working

--- public/js/quotes.js
+++ public/js/quotes.js
@@ -19,10 +19,11 @@ function slideQuotes () {
   }
   if (newQuote.school) citation += `<span class="nobreak">${newQuote.school}`
   if (newQuote.name) citation += "</cite>"
-  incoming.innerHTML = `<p>${newQuote.quote}</p>${citation}`
   outgoing.className = "offLeft";
   incoming.className = "centered";
   ondeck.className = "offRight";
+  ondeck.innerHTML = "";
+  incoming.innerHTML = `<p>${newQuote.quote}</p>${citation}`
 }
 
 function nextQuote () {
@@ -43,20 +44,20 @@ function initQuotes () {
     quote: "Having 32 seven-year-olds in a class is really tough! It means way more time is spent on behavior management and classroom management and way less time on teaching. It is nearly impossible to meet the needs of our students, especially the newcomers."
   })
   quotes.push ({
-    quote: "Our students now have to learn Spanish virtually, and we are outsourcing that to a third party. Our Sped/ELL instructor is stretched thin, and our school should be TBE. But we don’t have the funding for it. Our resources are scarce - each department gets 3 cases of paper for the year. We haven’t had department funds since before the pandemic - our chemistry teacher is buying her supplies out of pocket. We are the only school offering Aviation classes, but the district is dragging its feet in fixing our simulators."
+    quote: "Our resources are scarce. Each department gets three cases of paper for the year. We haven’t had department funds since before the pandemic. Our chemistry teacher is buying her supplies out of pocket."
   })
   quotes.push ({
     name: "Anita Guaman",
     school: "Orozco Elementary",
-    quote: "I have to diaper change my blended classes room, our bilingual kinder classroom has 28 students, teachers are being moved around, students not getting service."
+    quote: "I have to diaper change my blended classes room, our bilingual kindergarten classroom has 28 students, teachers are being moved around and students are not getting services."
   })
   quotes.push ({
-    quote: "I have 31 desks. 33 children. My room is a safety hazard. Students nor teacher can safely navigate the room due to congestion. 1st grade room 35 Students. Teacher has to utilize another room in order to provide instructions that require use of the rug."
+    quote: "I have 31 desks. 33 children. My room is a safety hazard. Neither students nor teacher can safely navigate the room due to congestion. 1st grade room 35 Students. Teacher has to utilize another room in order to provide instructions that require use of the rug."
   })
   quotes.push ({
     name: "Amaziah Burton",
     school: "Phillips HS",
-    quote: "We do not have enough TAs for our sped students who need dedicated assistants. There are sped students who should have a TA in Spanish classes according to their IEPs and are not receiving their services. There are SPED students in resource classes who don’t have their TA as mandated in their IEP. Bottom line: more TAs are needed to give our students the education they deserve!"
+    quote: "We do not have enough TAs for our sped students who need dedicated assistants. There are sped students who should have a TA in Spanish classes according to their IEPs and are not receiving their services."
   })
   quotes.push ({
     name: "Sarah Tschaen",
